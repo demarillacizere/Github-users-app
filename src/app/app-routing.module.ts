80b9faffd8990a48eae2917f505ea17293f9b9fc
Demarillac Izere <izered3@gmail.com>
2020-11-06 22:44:00 +0200

    added a 404 page for the website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { RepoComponent } from './repo/repo.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   { path: 'user', component: UserComponent},
   { path: 'repository', component: RepoComponent},
+  { path: '', redirectTo:"/goals", pathMatch:"full"},
+  { path:'**', component:NotFoundComponent},
 ];
 
 @NgModule({
