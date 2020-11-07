490c645fe76441070bc66d6cb976295a5bae0f46
Demarillac Izere <izered3@gmail.com>
2020-11-08 00:22:22 +0200

    edited the API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:"666cda213577c51bf28c3629b3f082d6d5e45af4"
 };
 
 /*
