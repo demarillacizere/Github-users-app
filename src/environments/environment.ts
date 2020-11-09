5486d68b2b3dac3e9b39a06b1376a1fd79e87476
Demarillac Izere <izered3@gmail.com>
2020-11-09 10:42:43 +0200

    removed the environment.ts from the gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiKey:"666cda213577c51bf28c3629b3f082d6d5e45af4"
 };
 
